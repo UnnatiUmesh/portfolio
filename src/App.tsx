6c32435e8bd0a7219007017c1f78e9897bfe1ea4
UnnatiUmesh <unnatiuk.2003@gmail.com>
2025-04-11 15:38:49 +0530

    Include App.tsx file

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Header from "./header/header";
+import "./App.css"
+import Projects from "./components/projects/Projects";
+import Intrests from "./components/intersets/Intrests";
+import Education from "./components/education/Education";
+import Contact from "./components/contacts/Contacts";
+import Footer from "./footer/Footer";
+
+function App() {
+  return (
+   <>
+    <Header />
+    <About />
+    <Experience />
+    <Projects />
+    <Intrests />
+    <Education />
+    <Contact />
+    <Footer />
+    </>
+  )
+}
+export default App;
