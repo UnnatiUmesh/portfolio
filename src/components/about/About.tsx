f5d308913d4b6e85ec2eb3b2a6c818fc8caec254
UnnatiUmesh <unnatiuk.2003@gmail.com>
2025-04-11 15:41:35 +0530

    Update about.tsx file

---
+++ src/components/about/About.tsx
@@ -0,0 +1,26 @@
+import "./About.css";
+
+const About = (): React.ReactNode => {
+  return (
+    <div className="about-container">
+      <div className="about-label">
+        <h2>About</h2>
+      </div>
+      <div className="about-text">
+        <p>I'm Unnati Umesh Kulkarni. I'm a full-stack mobile developer.</p>
+
+        <p>
+          While professionally specialised in minimalist, aesthetic and functional experiences, I
+          extend these principles to my personal life and beyond.
+        </p>
+
+        <p>
+          With a degree in Computer Science, I am enthusiastic to learn about new technologies.
+        </p>
+
+      </div>
+    </div>
+  );
+};
+
+export default About;
