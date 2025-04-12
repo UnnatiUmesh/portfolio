674f0860bec50bc7286d8e672499794aed632bca
UnnatiUmesh <unnatiuk.2003@gmail.com>
2025-04-12 16:46:31 +0530

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
 import Header from "./header/header";
-import "./App.css"
+import "./App.css";
 import Projects from "./components/projects/Projects";
 import Intrests from "./components/intersets/Intrests";
 import Education from "./components/education/Education";
@@ -10,16 +10,16 @@ import Footer from "./footer/Footer";
 
 function App() {
   return (
-   <>
-    <Header />
-    <About />
-    <Experience />
-    <Projects />
-    <Intrests />
-    <Education />
-    <Contact />
-    <Footer />
+    <>
+      <Header />
+      <About />
+      <Experience />
+      <Projects />
+      <Intrests />
+      <Education />
+      <Contact />
+      <Footer />
     </>
-  )
+  );
 }
 export default App;
